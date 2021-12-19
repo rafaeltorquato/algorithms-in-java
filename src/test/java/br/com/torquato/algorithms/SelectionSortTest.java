4787493b745b6beb9e1c7420b2a75e0d42e1b74b
Rafael Torquato <rafaeltorquat0@protonmail.com>
2021-12-19 15:11:05 -0300

    ♻️ Removed System.out.print

--- src/test/java/br/com/torquato/algorithms/SelectionSortTest.java
+++ src/test/java/br/com/torquato/algorithms/SelectionSortTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -23,7 +22,6 @@ class SelectionSortTest {
     public void shouldSortAnUnsortedArray() {
         Integer[] array = new Integer[]{10, 9, 8, 5, 7, 4, 6, 3, 2, 1};
         Integer[] sortedArray = this.selectionSort.sort(array);
-        System.out.println(List.of(sortedArray));
         assertTrue(Arrays.equals(sortedArray, new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
     }
 
