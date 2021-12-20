a9867d2cb222237d0342f97ea4730152e47ffb23
Rafael Torquato <rafaeltorquat0@protonmail.com>
2021-12-20 01:35:55 -0300

    ♻️ Minor test refactoring

--- src/test/java/br/com/torquato/algorithms/SelectionSortTest.java
+++ src/test/java/br/com/torquato/algorithms/SelectionSortTest.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class SelectionSortTest {
 
@@ -22,7 +20,7 @@ class SelectionSortTest {
     public void shouldSortAnUnsortedArray() {
         Integer[] array = new Integer[]{10, 9, 8, 5, 7, 4, 6, 3, 2, 1};
         Integer[] sortedArray = this.selectionSort.sort(array);
-        assertTrue(Arrays.equals(sortedArray, new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+        assertArrayEquals(sortedArray, new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
     }
 
 }
